5fc732b4d3f922af7d71b7590a8859373667514d
Federico Murrone <fedemurrone8@gmail.com>
2024-10-16 15:08:49 -0300

    Cambio de email a user

--- aplicacion/models/user.model.php
+++ aplicacion/models/user.model.php
@@ -8,7 +8,7 @@
         }
 
         public function getUserByEmail($email){
-            $query = $this->db->prepare("SELECT * FROM usuario WHERE email = ?");
+            $query = $this->db->prepare("SELECT * FROM usuario WHERE user = ?");
             $query->execute([$email]);
 
             $user = $query->fetch(PDO::FETCH_OBJ);
